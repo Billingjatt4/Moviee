db15abba77b0c67c697d7ce5e54cc1e73b8a24ff
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-27 20:08:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/badmunda011/Moviee.git /DQTheFileDonorBot
+  git clone https://github.com/badmunda011/Moviee.git /Pbxmoive_Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Pbxmoive_Bot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
